07f0acec6224bf08473b0a23a59e6c5903eb4cdc
root <root@srv1489383.hstgr.cloud>
2026-05-21 13:53:21 +0000

    Prevent cached 404 on detail pages

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,8 @@
 ﻿import { notFound } from 'next/navigation'
 
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export default function BlogDetailPage() {
   notFound()
 }
